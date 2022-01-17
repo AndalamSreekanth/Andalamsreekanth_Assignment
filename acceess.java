aa7d5ec640759dd75612e9d511ba132d31c67c61
AndalamSreekanth <97775869+AndalamSreekanth@users.noreply.github.com>
2022-01-17 10:51:36 +0530

    Add files via upload

---
+++ acceess.java
@@ -0,0 +1,25 @@
+package acceess;
+
+
+	//1. Class is having Default access modifier 
+	class defAccessSpecifier
+	{ 
+	void display() 
+	   { 
+	       System.out.println("You are using defalut access specifier"); 
+	   } 
+	} 
+
+	public class acceess {
+
+		public static void main(String[] args) {
+			//default
+			System.out.println("Dafault Access Specifier");
+			defAccessSpecifier obj = new defAccessSpecifier(); 		  
+	      obj.display(); 
+
+		}
+	}
+
+
+
