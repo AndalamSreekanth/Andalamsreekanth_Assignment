e071cf75342fca82435287e0d98e9954d0b2e40b
AndalamSreekanth <97775869+AndalamSreekanth@users.noreply.github.com>
2022-01-26 20:11:35 +0530

    Add files via upload

---
+++ mythread.java
@@ -0,0 +1,18 @@
+
+
+	public class mythread extends Thread
+	{
+	 	public void run()
+	 	{
+	  		System.out.println("concurrent thread started running..");
+	}
+	 	public static void main( String args[] )
+	 	{
+	  		mythread mt = new  mythread();
+	  		mt.start();
+	 	}
+	}
+
+
+
+
